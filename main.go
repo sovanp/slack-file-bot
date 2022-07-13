1c758c251e7850efd6086283abefc89149db6e26
Sovan Patnayak <sovanpatnayak@gmail.com>
2022-07-13 15:41:49 -0400

    New Bot Token

--- main.go
+++ main.go
@@ -7,13 +7,8 @@ import (
 )
 
 func main() {
-
-	// os.Setenv("SLACK_BOT_TOKEN", "xoxb-3761425391125-3796134192258-kJQNBU4uRv8EnsbbNhxEBvFz")
-	// os.Setenv("CHANNEL_ID", "")
-
 	//Variables to store token, channel id, and files
 	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
 	channelArr := []string{os.Getenv("CHANNEL_ID")}
 	fileArr := []string{""}
-
 }
